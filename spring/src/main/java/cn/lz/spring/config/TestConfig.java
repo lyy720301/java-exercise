ecf1d4d40a922212385ebbfa37717d502dafb3c4
lyy720301 <444109668@qq.com>
2022-06-01 15:35:57 +0800

    propertySource

--- spring/src/main/java/cn/lz/spring/config/TestConfig.java
+++ spring/src/main/java/cn/lz/spring/config/TestConfig.java
@@ -1,9 +1,21 @@
 package cn.lz.spring.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+@Slf4j
 @Configuration
 @PropertySource("classpath:test-config.properties")
-public class TestConfig {
+public class TestConfig implements CommandLineRunner {
+
+    @Value("${test.name}")
+    private String name;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("name {}", name);
+    }
 }
